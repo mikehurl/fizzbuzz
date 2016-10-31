48329ebc95f1554f0ac32127d174d43434d6427e
Mike Hurl <mikehurl@gmail.com>
2016-10-31 16:26:16 +0000

    two tests passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,13 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
     it 'returns "fizz" when passed a multiple of 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
         expect(fizzbuzz(6)).to eq 'fizz'
+    end
+    
+    it 'returns "buzz" when passed a multiple of 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+        expect(fizzbuzz(10)).to eq 'buzz'
     end
 end
