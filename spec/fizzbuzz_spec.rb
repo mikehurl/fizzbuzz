ec46a956e592ebb92ab0224cf543febd0704354b
Mike Hurl <mikehurl@gmail.com>
2016-10-31 16:34:06 +0000

    All tests passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,12 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(5)).to eq 'buzz'
         expect(fizzbuzz(10)).to eq 'buzz'
     end
+    it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+        expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    end
+    it 'returns the number if it is not a multiple of 3 or 5' do
+        expect(fizzbuzz(1)).to eq 1
+        expect(fizzbuzz(31)).to eq 31
+    end
 end
